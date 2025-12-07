d5bdc87ef4324dfd93e68a4674f1d3e3d7e83a73
MAV1Kk <mav1k.799@gmail.com>
2025-12-07 11:05:00 +0200

    feat: add prisma module integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
+import { PrismaModule } from './prisma/prisma.module.js';
+import { UsersModule } from './users/users.module.js';
+import { WorkoutsModule } from './workouts/workouts.module.js';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, UsersModule, WorkoutsModule],
   controllers: [AppController],
   providers: [AppService],
 })
